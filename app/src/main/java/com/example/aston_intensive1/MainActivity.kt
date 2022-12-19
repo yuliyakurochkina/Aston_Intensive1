1dec41fa1fe47cede7fa1a07b3aaff0cdf515768
Yuliya Kurochkina <yuliyakur2709@gmail.com>
2022-12-19 17:22:46 +0300

    color buttun in activity_main changed

--- app/src/main/java/com/example/aston_intensive1/MainActivity.kt
+++ app/src/main/java/com/example/aston_intensive1/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.aston_intensive1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnToSecondScreen = findViewById<Button>(R.id.btn_next_screen)
+        val intent = Intent(this, SecondActivity::class.java)
+
+        btnToSecondScreen.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
